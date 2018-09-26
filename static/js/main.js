4f88ecff505c88a62666babfd220bb290eb03d57
łukasz Strachanowski <lukasz.strachanowski@gmail.com>
2018-09-26 23:20:50 +0200

    menu background added , showing and hiding backround working

--- static/js/main.js
+++ static/js/main.js
@@ -1,15 +1,28 @@
 document.addEventListener('DOMContentLoaded', ()=>{
-    document.querySelector('.menu-style-wrapper').onclick = hide;
-    document.querySelector('.cross').onclick = hide;
+    document.querySelector('.menu-style-wrapper').onclick = hide_menu_icon;
+    document.querySelector('.cross').onclick = hide_menu_icon;
+    document.querySelector('.menu-active-background').onclick = hide_menu;
 })
 
-var hide =() =>{
+var hide_menu_icon =() =>{
     if(document.querySelector(".menu-style-wrapper").style.display=="" || document.querySelector(".menu-style-wrapper").style.display=="block"){
         document.querySelector(".menu-style-wrapper").style.display = "none";
         document.querySelector(".cross").style.display = "block"; 
+        document.querySelector(".menu-active-background").style.display = "block";
     }else{
         document.querySelector(".cross").style.display = "none"; 
         document.querySelector(".menu-style-wrapper").style.display = "block";
+        document.querySelector(".menu-active-background").style.display = "none";
     }
+
+
+
         
+}
+
+var hide_menu =()=>{
+    if(document.querySelector(".menu-style-wrapper").style.display==""){
+        document.querySelector(".menu-active-background").style.display = "none";
+    }
+    
 }
