e5f42aa63111b1d141219f40c08ea05a8c5a2344
łukasz Strachanowski <lukasz.strachanowski@gmail.com>
2018-09-26 21:58:16 +0200

    menu toggle is changing icon to cross when clicked

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,15 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+    document.querySelector('.menu-style-wrapper').onclick = hide;
+    document.querySelector('.cross').onclick = hide;
+})
 
+var hide =() =>{
+    if(document.querySelector(".menu-style-wrapper").style.display=="" || document.querySelector(".menu-style-wrapper").style.display=="block"){
+        document.querySelector(".menu-style-wrapper").style.display = "none";
+        document.querySelector(".cross").style.display = "block"; 
+    }else{
+        document.querySelector(".cross").style.display = "none"; 
+        document.querySelector(".menu-style-wrapper").style.display = "block";
+    }
+        
+}
