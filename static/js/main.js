74a7261cec633f6fd95b4a1249208da8ae05f7f8
łukasz Strachanowski <lukasz.strachanowski@gmail.com>
2018-10-13 11:39:47 +0200

    scroll event changed for slides

--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', ()=>{
     document.querySelector('.index-header-menu-align').onclick = hide_menu_icon;
-    document.onwheel =(e) = test;
+    window.addEventListener('wheel', test);
 });
 
 var test = (e)=>{
-    if(e.wheelDelta > 0){
+    if(e.deltaY < 0 ){
         document.getElementsByClassName("slide-one")[0].style.display = "block";
         document.getElementsByClassName("slide-two")[0].style.display = "none";
         document.getElementsByClassName("dot-menu")[0].style.background = "#C1292F";
@@ -17,9 +17,7 @@ var test = (e)=>{
     }
 }
 
-var myFunction = ()=>{
-    console.log('proint');
-}
+
 var hide_menu_icon =() =>{
     if(document.querySelector(".menu-style-wrapper").style.display=="" || document.querySelector(".menu-style-wrapper").style.display=="block"){
         document.querySelector(".menu-style-wrapper").style.display = "none";
